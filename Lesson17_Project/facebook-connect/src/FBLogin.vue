55aa01d90799f50ab9b515e4113c14ae77acc8c9
Mars <talent.seek@msa.hinet.net>
2018-04-12 18:24:18 +0800

    20180412-3

---
+++ Lesson17_Project/facebook-connect/src/FBLogin.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    1234
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    FB.init({
+      appId: '236323106934165',
+      version: "v2.12",
+    });
+
+    FB.getLoginStatus(result => {
+      console.log("ccccc");
+      console.log(result);
+    });
+
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ Lesson17_Project/facebook-connect/src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import FBLogin from './FBLogin.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(FBLogin)
+})
